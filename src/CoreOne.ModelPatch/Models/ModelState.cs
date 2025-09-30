fb05f85b36fe2c8d7c6da10ee37a43be14bfc680
Juan Lopez <lopez5@hotmail.com>
2025-09-29 23:37:08 -0400

    Updating nuget version

--- src/CoreOne.ModelPatch/Models/ModelState.cs
+++ src/CoreOne.ModelPatch/Models/ModelState.cs
@@ -1,9 +1,31 @@
-﻿namespace CoreOne.ModelPatch.Models;
+﻿using System.Diagnostics;
+
+namespace CoreOne.ModelPatch.Models;
 
 /// <summary>
 ///
 /// </summary>
 /// <typeparam name="T"></typeparam>
-/// <param name="Model"></param>
-/// <param name="CrudType"></param>
-public record ModelState<T>(T Model, CrudType CrudType);
+/// <param name="model"></param>
+/// <param name="crudType"></param>
+[DebuggerDisplay("Crud: {CrudType}... Type: {ModelType}")]
+public class ModelState<T>(T model, CrudType crudType)
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public CrudType CrudType { get; init; } = crudType;
+    /// <summary>
+    ///
+    /// </summary>
+    public T Model { get; init; } = model;
+    protected string ModelType { get; init; } = typeof(T).Name;
+}
+
+public class ModelState : ModelState<object>
+{
+    public ModelState(object model, CrudType crudType) : base(model, crudType)
+    {
+        ModelType = model.GetType().Name;
+    }
+}
